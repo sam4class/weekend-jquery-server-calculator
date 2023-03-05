9bf807d3f32416c04da60be6282d49220eebc357
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-03-05 14:16:02 -0600

    pushing at a semi broken spot

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,30 +1,56 @@
+
+
 $(document).ready(onReady);
+
+let answer = 0;
+let sign = '';
 
 function onReady(){
     console.log('This times the charm.');
 
     $("#equals").on('click', addAllThatInfo);
 
+    //find out sign
+   $('#add').on('click', toAdd);
+   $('#minus').on('click', toMinus);
+   $('#multiply').on('click', toMulti);
+   $('#divide').on('click', toDivide);
+
     getThatInfo();
 }
 
+let newNum1 = $("#number1").val();
+let newNum2 = $("#number2").val();
+
+console.log ('these are num1 and num2', newNum1, newNum2);
+
 function addAllThatInfo(){
     //get values from inputs
+    // let num1 = $("#number1").val();
+    // let num2 = $("#number2").val();
+    // console.log ('these are num1 and num2', num1, num2);
+
     let info = {
-        num1: $("#number1").val(),
-        num2: $("#number2").val()
+        num1:$("#number1").val(),
+        num2: $("#number2").val(),
+        sign: sign,
+        answer: answer
     }
     console.log(info);
     //ajax to server
 
     $.ajax({
         method: 'POST',
-        url: '/allInfo',
+        url: '/infoNeeded',
         data: info, //object
     }).then((response) =>{
         console.log('post finished');
+        //update with your GET
+        getThatInfo();
+       
     })
     //deal with response
+   // render();
 }
 
 function getThatInfo(){ //get this in onReady
@@ -39,5 +65,45 @@ function getThatInfo(){ //get this in onReady
 
     }).catch(function(){
         alert('failed at getThatInfo');
-    })
+    });
 }
+
+function toAdd(){
+    sign = '+';
+      console.log(sign);
+  }
+
+  function toMinus(){
+      sign = '-';
+     console.log(sign);
+  }
+
+  function toMulti(){
+      sign = '*';
+      console.log(sign);
+  }
+  function toDivide(){
+      sign = '/';
+      console.log(sign);
+  }
+
+  function render(response){
+        console.log('Inside render')
+    
+        $('calSetUp').empty();
+        $('#answer').empty();
+    
+        $('#answer').append(`
+        <h2 id="answer">${answer}</h2>`);
+    
+
+        $('#addedData').append(`    
+        <div id="pastAnswerList">
+            <ul>
+                <li>${num1}${sign}${num2}=${answer}</li>
+            </ul>
+        </div>
+        
+        `)
+    }
+
--- server/server.js
+++ server/server.js
@@ -7,17 +7,72 @@ const PORT = 8000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('server/public'));  //#1 don't forget the port listen
 
-let infoList = [
-    {number1: '', sign: '', number2: '', answer: '', history: []}
-];
+let answer = 0;
+let sign = '';
+
+let infoList = [{number1: '', sign: "", number2: '', answer: '', history: []}];
 
 app.get('/infoNeeded', (req,res) => {
     console.log('request made');
     res.send(infoList);
 })
 
-app.post('/allInfo', (req,res) => {
+app.post('/infoNeeded', (req,res) => {
     console.log('post a post', req.body);
+
+    //accessing the info in post client side
+    //data will  be put on req.body
+    //exact same object as the client side
+
+//check how to calculate
+
+let num1 = Number(req.body.num1);
+let num2 = Number(req.body.num2);
+let sign = req.body.sign;
+let answer = req.body.answer;
+
+let allInfo = [{
+    number1: num1,
+    number2: num2,
+    sign: sign,
+    answer: answer
+}];
+
+if(sign === '+'){
+    answer = num1 + num2;
+    console.log('add answer', answer);
+    console.log('sign', sign);
+}
+
+if(sign === '-'){
+    answer = num1 - num2;
+    console.log('add answer', answer); 
+    console.log('sign', sign);
+}
+
+if(sign === '*'){
+    answer = num1 * num2;
+    console.log('add answer', answer);
+    console.log('sign', sign);
+}
+if(sign === '/'){
+    answer = num1 / num2;
+    console.log('add answer', answer);
+    console.log('sign', sign);
+   
+}
+infoList.push(allInfo);
+
+
+
+
+//store the numbers
+// infoList.history.push(req.body.num1);
+// infoList.history.push(req.body.num2);
+// console.log(infoList);
+
+
+
 
 
 
