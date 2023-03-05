4a6ee16d49764f6658d7401a0c6429fa17daec8a
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-03-05 11:29:07 -0600

    restarted with video setUp done and get post added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,114 +1,43 @@
-
-
 $(document).ready(onReady);
 
-let answer = 0;
-let sign = '';
-
-
 function onReady(){
-    console.log('Time to do this');
-    getTwoNumbers();   //#4
+    console.log('This times the charm.');
 
-    $('#equals').on('click', grabNumber); //#5
- 
-    //find out sign
-    $('#add').on('click', toAdd);
-    $('#minus').on('click', toMinus);
-    $('#multiply').on('click', toMulti);
-    $('#divide').on('click', toDivide);
+    $("#equals").on('click', addAllThatInfo);
 
-     
+    getThatInfo();
 }
 
-function grabNumber(){  //#5.5
-    //what this is doing:
-    //get values from input
-    //ajax to server
-    //deal with response
-
-    //get vaules by making an object
-    let nowNumbers ={
-        num1: Number($('#number1').val()),
-        num2: Number($('#number2').val()),
-        
+function addAllThatInfo(){
+    //get values from inputs
+    let info = {
+        num1: $("#number1").val(),
+        num2: $("#number2").val()
     }
-    
-   console.log(nowNumbers);
+    console.log(info);
     //ajax to server
+
     $.ajax({
         method: 'POST',
-        url: '/numberGrab',
-        data:{
-          nowNumbers, //must be an objest, the one you just made in this post
-         }
-    }).then((repsonse) => {
-        console.log('Post posted!')
-        num1 = Number($('#number1').val(''));
-        num2 = Number($('#number2').val(''))
-        //update our data
-        getTwoNumbers();
-        render(); 
-    });
+        url: '/allInfo',
+        data: info, //object
+    }).then((response) =>{
+        console.log('post finished');
+    })
     //deal with response
- 
-       
-    }
+}
 
-    function toAdd(){
-        sign = '+';
-        console.log(sign);
-    }
-
-    function toMinus(){
-        sign = '-';
-       console.log(sign);
-    }
-
-    function toMulti(){
-        sign = '*';
-        console.log(sign);
-    }
-    function toDivide(){
-        sign = '/';
-        console.log(sign);
-    }
-    
-
-
- 
-function getTwoNumbers(){ //#3
+function getThatInfo(){ //get this in onReady
+    console.log('Inside getThatInfo');
 
     $.ajax({
         method: 'GET',
-        url: '/numbers'
-    }).then ((response) => {
-        console.log('Inside getTwoNumbers', response);
-        
+        url: '/infoNeeded'
+    }).then((response) => {
+        console.log('results from get', response);
+        //will update with render?
+
     }).catch(function(){
-        alert('failed at grabTwoNumbers');
+        alert('failed at getThatInfo');
     })
-
-}
-
- 
-
-
-function render(response){
-    console.log('Inside render')
-
-    $('calSetUp').empty();
-    $('#answer').empty();
-
-    $('#answer').append(`
-    <h2 id="answer">${answer}</h2>`);
-
-    $('#addedData').append(`    
-    <div id="pastAnswerList">
-        <ul>
-            <li>${num1}${sign}${num2}=${answer}</li>
-        </ul>
-    </div>
-    
-    `)
 }
--- server/server.js
+++ server/server.js
@@ -5,90 +5,26 @@ const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('server/public'));
+app.use(express.static('server/public'));  //#1 don't forget the port listen
 
-let answer = 0;
-let sign = '';
+let infoList = [
+    {number1: '', sign: '', number2: '', answer: '', history: []}
+];
 
-let twoNumbers = { //#1
-    inputNum1: '',
-    inputNum2: '',
-    sign: '', 
-    answer: '',
-    history: [] //will hold history
-};
-
-
-
-app.get('/numbers', function(req, res){ //#2
-    console.log('Request was made');
-    res.send(twoNumbers);
+app.get('/infoNeeded', (req,res) => {
+    console.log('request made');
+    res.send(infoList);
 })
 
-app.post('/numberGrab', (req,res) => {  //#6
+app.post('/allInfo', (req,res) => {
     console.log('post a post', req.body);
 
-    //incoming value of two numbers
-    let num1 = Number(req.body.num1);
-    let num2 = Number(req.body.num2);
-    // let newSign = req.body.sign;
-    // let newAnswer = req.body.answer;    
 
 
-    // console.log(num1, num2, newSign, newAnswer);
-
-    // let newTwoNumbers = {
-    //     inputNum1: newInputNum1,
-    //     inputNum2: newInputNum2,
-    //     sign: newSign,
-    //     answer: newAnswer,
-    //     history: [],
-    
-    // }
-
-    // twoNumbers.push(newTwoNumbers);
-
-    //store the numbers
-    twoNumbers.history.push(req.body.num1);
-    twoNumbers.history.push(req.body.num2);
-
-//    // do calculations?
-if(sign === '+'){
-    answer = num1 + num2;
-    console.log('add answer', answer);
-    twoNumbers.answer = answer;
-   
-}
-
-if(sign === '-'){
-    answer = num1 - num2;
-    console.log('add answer', answer);
-    twoNumbers.answer = answer;
-    
-}
-
-if(sign === '*'){
-    answer = num1 * num2;
-    console.log('add answer', answer);
-    twoNumbers.answer = answer;
-   
-}
-if(sign === '/'){
-    answer = num1 / num2;
-    console.log('add answer', answer);
-    twoNumbers.answer = answer;
-}
-twoNumbers.history.push(req.body.answer);
-
-    //store the numbers
-twoNumbers.history.push(req.body.num1);
-twoNumbers.history.push(req.body.num2);
-
-res.sendStatus(201);
-});
-
-
+    res.sendStatus(201); //res.send(infoList); 
+})
 
 app.listen(PORT, () => {
     console.log('Server is running', PORT);
-})
+});
+
